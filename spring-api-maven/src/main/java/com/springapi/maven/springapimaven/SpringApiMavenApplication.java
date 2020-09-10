9a44756cb05b22e8525df9fc9c166441647cfb39
arjun-526 <nagarjuna.anke3@gmail.com>
2020-09-11 03:19:15 +0530

    first commit

---
+++ spring-api-maven/src/main/java/com/springapi/maven/springapimaven/SpringApiMavenApplication.java
@@ -0,0 +1,13 @@
+package com.springapi.maven.springapimaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiMavenApplication.class, args);
+	}
+
+}
---
+++ spring-api-maven/src/test/java/com/springapi/maven/springapimaven/SpringApiMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.maven.springapimaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
